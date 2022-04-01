23dc4fbb9bbd3fb7b8dd19fe5b27e13363021d6c
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2022-04-01 18:29:41 +0200

    Updates

--- serviceworker.js
+++ serviceworker.js
@@ -9,31 +9,32 @@ self.addEventListener('install', async (e) => {
     await cache.addAll(urlToCache);
     await self.skipWaiting();
 })
-console.log(this);
 //listen for requests
 this.addEventListener('fetch', async function (e) {
-    // console.log("test-offline");
-    if (!navigator.onLine) {
-        // console.log("test-offline");
-        return await e.respondWith(await caches.match(e.request) || await caches.match('/notfound'))
-    } else {
-        // console.log("test online");
-        return await e.respondWith(networkFirst(e.request));
-    }
+    // // console.log("test-offline");
+    // if (!navigator.onLine) {
+    //     // console.log("test-offline");
+    //     return await e.respondWith(await caches.match(e.request) || await caches.match('/notfound'))
+    // } else {
+    //     // console.log("test online");
+    //     return await e.respondWith(networkFirst(e.request));
+    // }
+    e.respondWith(
+        caches.match(e.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(e.request);
+            }
+            )
+    );
 })
 
 //Activate SW
 self.addEventListener('activate', async (e) => {
-    // const cacheWhiteList = [];
-    // cacheWhiteList.push(CASH_NAME);
-    // cacheWhiteList.push('dynamic-cache');
-    // let allCaches = await caches.keys();
-    // allCaches.map((cacheName) => {
-    //     if (!cacheWhiteList.includes(cacheName)) {
-    //         return caches.delete(cacheName)
-    //     }
-    // })
-    // console.log('Claiming control');
+
     return self.clients.claim();
 })
 
